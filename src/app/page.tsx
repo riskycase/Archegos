45f5e58a0596c099374f39fc94a78eb0e7bd3029
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2024-07-22 15:36:35 +0530

    3xl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             </p>
           </div>
           <div className="flex flex-col mb-40 justify-between items-center space-x-0 md:space-x-10 md:mb-0 md:flex-row">
-  <h1 className="max-w-4xl pt-10 text-3xl font-bold text-center md:text-left md:text-4xl lg:text-4xl">
+  <h1 className="max-w-3xl pt-10 text-3xl font-bold text-center md:text-left md:text-4xl lg:text-4xl">
     <div className="text-red-800">
       At Archegos, we bridge the gap{" "}
     </div>{" "}
