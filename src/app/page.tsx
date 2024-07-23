292782b272037428e24d832eedde6e5e3bdb9a36
raiiasingh19 <raiia.singh1@gmail.com>
2024-07-23 22:14:30 +0530

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="h-fit">
         <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
           <div className="mx-auto mb-4 mt-10 flex flex-row max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-            <p className="text-sm font-semibold text-gray-700">
+            <p className="text-xs sm:text-sm font-semibold text-gray-700">
               High-end medical care, at your doorstep now!
             </p>
           </div>
